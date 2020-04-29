dde23c0a65ed75c22cabd9a69b44cdec0e2323e3
Ollin <ollin.demian@gmail.com>
2020-04-29 18:24:09 -0400

    completely change the pipeline name and clean rute of conapo download script

--- src/ingest/conapo.sh
+++ src/ingest/conapo.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-wget -O ../data/conapo_1.rar http://www.conapo.gob.mx/work/models/CONAPO/Datos_Abiertos/Proyecciones2018/base_municipios_final_datos_01.rar
-wget -O ../data/conapo_2.rar http://www.conapo.gob.mx/work/models/CONAPO/Datos_Abiertos/Proyecciones2018/base_municipios_final_datos_02.rar
+wget -O ../data/raw/conapo_1.rar http://www.conapo.gob.mx/work/models/CONAPO/Datos_Abiertos/Proyecciones2018/base_municipios_final_datos_01.rar
+wget -O ../data/raw/conapo_2.rar http://www.conapo.gob.mx/work/models/CONAPO/Datos_Abiertos/Proyecciones2018/base_municipios_final_datos_02.rar
 
-unrar x ../data/conapo_1.rar ../data/
-unrar x ../data/conapo_2.rar ../data/
+unrar x ../data/raw/conapo_1.rar ../data/raw/
+unrar x ../data/raw/conapo_2.rar ../data/raw/
 
 newline="CLAVE,CLAVE_ENT,NOM_ENT,MUN,SEXO,ANO,EDAD_QUIN,POB"
 
-cat ../data/base_municipios_final_datos_01.csv | sed '1d' | sed '1i RENGLON,CLAVE,CLAVE_ENT,NOM_ENT,MUN,SEXO,ANO,EDAD_QUIN,POB' | cut -d"," -f2- > ../data/conapo_1.csv
+cat ../data/raw/base_municipios_final_datos_01.csv | sed '1d' | sed '1i RENGLON,CLAVE,CLAVE_ENT,NOM_ENT,MUN,SEXO,ANO,EDAD_QUIN,POB' | cut -d"," -f2- > ../data/raw/conapo_1.csv
 
-cat ../data/base_municipios_final_datos_02.csv | sed '1d' |  cut -d"," -f2- > ../data/conapo_2.csv
+cat ../data/raw/base_municipios_final_datos_02.csv | sed '1d' |  cut -d"," -f2- > ../data/raw/conapo_2.csv
 
-cat ../data/conapo_1.csv ../data/conapo_2.csv > ../data/conapo.csv
+cat ../data/raw/conapo_1.csv ../data/raw/conapo_2.csv > ../data/clean/conapo.csv
 
-rm ../data/conapo_1.rar ../data/conapo_2.rar\
-    ../data/base_municipios_final_datos_02.csv\
-    ../data/base_municipios_final_datos_01.csv ../data/conapo_1.csv ../data/conapo_2.csv
+rm ../data/raw/*
