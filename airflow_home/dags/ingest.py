4051fbf2827df12bfb10e1a57bb68ada2391c88d
Ollin <ollin.demian@gmail.com>
2020-04-30 12:16:29 -0400

    moving from airflow bash tasks to docker tasks

--- airflow_home/dags/ingest.py
+++ airflow_home/dags/ingest.py
@@ -3,12 +3,17 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.docker_operator import DockerOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
 
 default_args = {
         'owner': 'airflow',
         'depends_on_past': False,
-        'start_date': datetime(2020,4,29),
+        # If we want to select a specific launch date
+        # 'start_date': datetime(2020,4,29),
+        'start_date': days_ago(2),
+        'email': ['olangle@uvm.edu'],
         'email_on_failure': False,
         'email_on_retry': False,
         'concurrency': 1,
@@ -16,26 +21,52 @@ default_args = {
         'retries': 0,
         }
 
-dag = DAG('simple', default_args=default_args,
-        schedule_interval= '*/5 * * * *'
-        )
+dag = DAG(
+    'ingest',
+    default_args=default_args,
+    description='Ingestion of the required files',
+    schedule_interval=timedelta(days=1),
+#     This can also be a common cron job
+#     schedule_interval= '*/5 * * * *'
+)
 
-opr_ingest = BashOperator(
-        task_id='ingest',
-        bash_command='python \
-        /home/ollin/Documentos/migration/migration-networks/common/ingest/src/asylum_seekers.py',
+conapo_ingest = DockerOperator(
+        task_id='conapo',
+        image='ingest',
+        command='/src/conapo.sh', \
         dag = dag)
 
-opr_delimiter = BashOperator(
-        task_id='delimiter',
-        bash_command='sh \
-        /home/ollin/Documentos/migration/migration-networks/common/ingest/src/delimiter.sh ',
+indigenous_ingest = BashOperator(
+        task_id='indigenous',
+        bash_command='docker run \
+                -v $(pwd)/data/:/data\
+                -v $(pwd)/src/ingest/:/src\
+                        ingest /src/indigenous_language.sh', \
         dag = dag)
 
-opr_graph = BashOperator(
-        task_id='graph',
-        bash_command='zsh \
-        /home/ollin/Documentos/migration/migration-networks/common/ingest/src/to_graph.sh ',
+marginalization_ingest = BashOperator(
+        task_id='marginalization',
+        bash_command='docker run \
+                -v $(pwd)/data/:/data\
+                -v $(pwd)/src/ingest/:/src\
+                        ingest /src/marginalization.sh', \
         dag = dag)
 
-opr_ingest >> opr_delimiter >> opr_graph
+schools_ingest = BashOperator(
+        task_id='schools',
+        bash_command='docker run \
+                -v $(pwd)/data/:/data\
+                -v $(pwd)/src/ingest/:/src\
+                        ingest /src/marginalization.sh', \
+        dag = dag)
+
+geoms_ingest = BashOperator(
+        task_id='geoms',
+        bash_command='docker run \
+                -v $(pwd)/data/:/data\
+                -v $(pwd)/src/ingest/:/src\
+                        ingest /src/marginalization.sh', \
+        dag = dag)
+
+
+conapo_ingest >> indigenous_ingest >> marginalization_ingest >> schools_ingest >> geoms_ingest
